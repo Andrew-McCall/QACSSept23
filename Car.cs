e5d89f5fb06a1efe509dc6117378811fbe53307a
Work <andrewdavidmccall@hotmail.com>
2023-09-26 13:23:33 +0100

    // Overloading // Manual Implentation // Private vs Public // Main Method

--- Car.cs
+++ Car.cs
@@ -9,11 +9,30 @@ namespace ConsoleApp3
     public class Car
     {
 
-        public int Speed { set; get; }
+        // Properties
+        private int Speed;
+
+        // Manual (Custom) implentation
+        public int GetSpeed()
+        {
+            return Speed;
+        }
+
+        public void SetSpeed(int speed)
+        {
+            if (speed > 0 && speed < 200)
+            {
+                Speed = speed;
+            }
+        }
+
+        // Auto-implentated. 
         public string Model { set; get; }
         public string Make { set; get; }
-        public string Reg { set; get; }
+        // Auto-implentated with setter restrictions
+        public string Reg { protected set; get; }
 
+        // Constructor
         public Car(string model, string make, string reg)
         {
             Model = model;
@@ -22,9 +41,26 @@ namespace ConsoleApp3
             Speed = 0;
         }
 
+        // Overloaded 
+        public Car(string reg) {
+            Reg = reg;
+            Speed = 0;
+            Make = "Nissan";
+            Model = "Micra";
+        }
+
+        // Methods
         public int Accelerate(int power)
         {
+            Console.WriteLine("int");
             Speed += power;
+            return Speed;
+        }
+
+        public int Accelerate(double power)
+        {
+            Console.WriteLine("double");
+            Speed += (int)power;
             return Speed;
         }
 
--- Program.cs
+++ Program.cs
@@ -1,31 +1,59 @@
 ﻿
-using ConsoleApp3;
+//using ConsoleApp3; // import everything that is in said namespace
+// this file is in a "top-level namespace"
 
-Car carOne = new Car("Leaf", "Nissan", "ABC");
+// Typically the name of your project. 
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        // Default Method/Entry Point
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
 
-Console.WriteLine(carOne.Reg);
-Console.WriteLine(carOne.Speed);
+            Car carOne = new Car("Leaf", "Nissan", "ABC");
 
-carOne.Accelerate(7);
+            Console.WriteLine(carOne.Reg);
+            Console.WriteLine(carOne.GetSpeed());
 
-Console.WriteLine(carOne.Speed);
+            carOne.Accelerate(7);
 
-Car carTwo = new Car("Leaf", "Nissan", "DFE");
-Car carThree = new Car("F150", "Ford", "123");
+            Console.WriteLine(carOne.GetSpeed());
 
-Console.WriteLine(carTwo.Reg);
-Console.WriteLine(carThree.Reg);
+            Car carTwo = new Car("Leaf", "Nissan", "DFE");
+            Car carThree = new Car("F150", "Ford", "123");
 
-carThree.Accelerate(451);
-carThree.Accelerate(124);
+            Console.WriteLine(carTwo.Reg);
+            Console.WriteLine(carThree.Reg);
 
-Console.WriteLine(carTwo.Speed);
-Console.WriteLine(carThree.Speed);
+            carThree.Accelerate(451);
+            carThree.Accelerate(124);
 
-carThree.EmergencyBrake();
+            Console.WriteLine(carTwo.GetSpeed());
+            Console.WriteLine(carThree.GetSpeed());
 
-Console.WriteLine(carThree.Speed);
+            carThree.EmergencyBrake();
 
-Console.WriteLine(carTwo.Accelerate(52));
-Console.WriteLine(carTwo.Accelerate(532));
-Console.WriteLine(carTwo.Speed);
+            Console.WriteLine(carThree.GetSpeed());
+
+            Console.WriteLine(carTwo.Accelerate(52));
+            Console.WriteLine(carTwo.Accelerate(532));
+            Console.WriteLine(carTwo.GetSpeed());
+
+            Car carFour = new Car("abc");
+            Console.WriteLine(carFour.Reg);
+            Console.WriteLine(carFour.Make);
+            Console.WriteLine(carFour.GetSpeed());
+            Console.WriteLine(carFour.Model);
+
+            carFour.Accelerate(7);
+            carFour.Accelerate(7.0);
+            carFour.Accelerate(7);
+
+
+            // carFour.GetSpeed() = 523;
+        }
+
+    }
+}
