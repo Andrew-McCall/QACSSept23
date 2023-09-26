82dd7ca0f2939e0b8f69764598a78a7755f92d5a
Work <andrewdavidmccall@hotmail.com>
2023-09-26 11:32:52 +0100

    Car Example

---
+++ Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Car
+    {
+
+        public int Speed { set; get; }
+        public string Model { set; get; }
+        public string Make { set; get; }
+        public string Reg { set; get; }
+
+        public Car(string model, string make, string reg)
+        {
+            Model = model;
+            Make = make;
+            Reg = reg;
+            Speed = 0;
+        }
+
+        public int Accelerate(int power)
+        {
+            Speed += power;
+            return Speed;
+        }
+
+        public void EmergencyBrake()
+        {
+            Speed = 0;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,86 +1,31 @@
-﻿// >  Greater
-// <  Lesser
-// == Equal
-// != Not Equal
-// >= Greater OR Equal
-// <= Lesser OR Equal
+﻿
+using ConsoleApp3;
 
-// && AND 
-// || OR
+Car carOne = new Car("Leaf", "Nissan", "ABC");
 
-// !  Make a true = false, false = true
+Console.WriteLine(carOne.Reg);
+Console.WriteLine(carOne.Speed);
 
-int x = 0;
-while (x < 1000)
-{
-    Console.WriteLine(x);
-    x = x + 3;
+carOne.Accelerate(7);
 
-    x++;       // + 1
+Console.WriteLine(carOne.Speed);
 
-    x += 3;
-};
+Car carTwo = new Car("Leaf", "Nissan", "DFE");
+Car carThree = new Car("F150", "Ford", "123");
 
-string greeting = "";
-while (greeting.Length < 30)
-{
-    Console.WriteLine(greeting);
-    greeting += "H ";
-};
+Console.WriteLine(carTwo.Reg);
+Console.WriteLine(carThree.Reg);
 
-while (true)
-{
-    Console.WriteLine("While True");
+carThree.Accelerate(451);
+carThree.Accelerate(124);
 
-    x += 5;
-    if (x > 200)
-    {
-        break; // can exit any loop.
-    }
-   
-};
+Console.WriteLine(carTwo.Speed);
+Console.WriteLine(carThree.Speed);
 
-for (int i = 10; i > 0; i--)
-{
-    Console.WriteLine(i);
-};
+carThree.EmergencyBrake();
 
-for (int counter = 1; counter % 7 != 0; counter += 2)
-{
-    Console.WriteLine(counter);
-};
+Console.WriteLine(carThree.Speed);
 
-for (int i = 0; i < 50; i++)
-{
-    Console.WriteLine(i);
-    i += 10;
-}
-
-// print out 0 to 10
-// print out 100 to 0 counting backwards in 9
-
-// play 99 green bottles on the wall
-// there are 99 green bottle on the wall and if one were to fall, there would be 98 bottles on the wall.
-// no negative print outs
-
-for (int i = 99; i > 0; i--)
-{
-    // $"hello {nameVar}" = "hello "+nameVar
-    if (i == 1)
-    {
-        Console.WriteLine($"There is 1 green bottle on the wall, And if that one were to fall, Then there would be no more bottles on the wall.");
-    }
-    else
-    {
-        Console.WriteLine($"There are {i} green bottles on the wall, And if one were to fall, Then there would be {i - 1} greens bottles on the wall.");
-    }
-}
-
-/*
-for (int i = 99; i >= 2; i--)
-{
-    // $"hello {nameVar}" = "hello "+nameVar
-    Console.WriteLine($"There are {i} green bottles on the wall, And if one were to fall, Then there would be {i - 1} greens bottles on the wall.");
-};
-Console.WriteLine($"There is 1 green bottle on the wall, And if that one were to fall, Then there would be no more bottles on the wall.");
-*/
+Console.WriteLine(carTwo.Accelerate(52));
+Console.WriteLine(carTwo.Accelerate(532));
+Console.WriteLine(carTwo.Speed);
