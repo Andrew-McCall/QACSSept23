273def3708a95a85816037b0c2c73480fd922888
Work <andrewdavidmccall@hotmail.com>
2023-09-28 11:38:31 +0100

    Garage Exercise complete

---
+++ Garage.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Garage
+    {
+
+        private Vehicle[] _vehicles;
+
+        public Garage(int length)
+        {
+            _vehicles = new Vehicle[length];
+        }
+
+        public Vehicle? Store(int index, Vehicle vehicle)
+        {
+            Vehicle? prev = _vehicles[index];
+            _vehicles[index] = vehicle;
+            return prev;
+        }
+
+        public void PrintAll()
+        {
+            foreach (Vehicle vehicle in _vehicles)
+            {
+                if (vehicle != null)
+                {
+                    Console.WriteLine(vehicle.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Empty Slot");
+                }
+            }
+        }
+
+        public Vehicle? Get(int id)
+        {
+            Vehicle? found = null;
+
+            foreach (Vehicle vehicle in _vehicles)
+            {
+                //if (vehicle == null) continue;
+                if (vehicle != null && vehicle.ID == id)
+                {
+                    found = vehicle;
+                    break; // Quits loop and doesnt search other vehicles
+                }
+            }
+
+            return found;
+        }
+
+        public void UseAll()
+        {
+            foreach (Vehicle vehicle in _vehicles)
+            {
+                if (vehicle != null)
+                {
+
+                    Console.WriteLine(vehicle.ToString());
+
+                    Boat? b = vehicle as Boat;
+                    if (b != null)
+                    {
+                        Console.WriteLine($"Price: {b.GetPrice()}");
+                        continue;
+                    }
+
+                    SunRoofCar? src = vehicle as SunRoofCar;
+                    if (src != null)
+                    {
+                        Console.WriteLine(src.IsRoofOpen);
+                        continue;
+                    }
+                    
+                    Car? c = vehicle as Car;
+                    if (c != null)
+                    {
+                        Console.WriteLine(c.Reg);
+                        c.Drive();
+                        continue;
+                    }
+
+                    // Each type
+                }
+            }
+        }
+
+        public void AddSlot()
+        {
+
+            Vehicle[] prev = _vehicles;
+            _vehicles = new Vehicle[prev.Length + 1];
+
+            for (int i = 0; i< prev.Length; i++)
+            {
+                _vehicles[i] = prev[i];
+            }
+
+        }
+
+        public Vehicle? RemoveVehicle(int index)
+        {
+            Vehicle? prev = _vehicles[index];
+            _vehicles[index] = null;
+            return prev;
+        }
+
+        public int? StoreVehicle(Vehicle vehicle)
+        {
+            int? index = null;
+
+            for (int i = 0; i < _vehicles.Length; i++)
+            {
+                if (_vehicles[i] is null)
+                {
+                    _vehicles[i] = vehicle;
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,72 +7,6 @@ namespace ConsoleApp3
         public static void Main(string[] args)
         {
 
-            // Array (Immuatable)
-            int[] myNumbers = { 1, 2, 3, 4, 5 };
-
-            Console.WriteLine(myNumbers);
-            Console.WriteLine(myNumbers[0]); // Index of an array starts at 0
-            Console.WriteLine(myNumbers[4]);
-            Console.WriteLine(myNumbers[3]);
-
-            myNumbers[0] = -100;
-            Console.WriteLine(myNumbers[0]);
-
-            Boat[] boats = new Boat[10]; // Create an empty arrray of 10 nulls
-            Console.WriteLine(boats[0]);
-            boats[0] = new Boat();
-            Console.WriteLine(boats[0]);
-
-            int[] empty = new int[8]; // eight 0s
-
-            // eight slots in an array, each holding another array
-            // Each row can hold different sized arrays
-            int[][] ints = new int[8][];
-
-            // {[{1,2,3,4,5}],[],[],[],[],[],[],[],[]}
-            // int[0][3] = 4;
-
-            // Fixed Width Mutlidimentional arra
-            // All of the arrays in the first array, have the same length
-            int[,] intt2 = new int[5, 4];
-
-            // {[{0,0,0,0}],[{0,0,0,0}],[{0,0,0,0}],[{0,0,0,0}],[{0,0,0,0}]}
-
-            ints[5] = new int[99];
-
-            Console.WriteLine(ints[5][52]);
-            Console.WriteLine(intt2[3,3]);
-
-            ints[5][52] = 532;
-            intt2[3, 3] = 812312;
-
-            Console.WriteLine(ints[5][52]);
-            Console.WriteLine(intt2[3, 3]);
-
-            Vehicle[] vehicles = new Vehicle[3];
-            vehicles[0] = new Boat();
-            vehicles[1] = new Car("abc");
-            vehicles[2] = new FishingBoat();
-
-            Boat? b = vehicles[0] as Boat;
-            if (b is not null)
-            {
-                Console.WriteLine(b.GetPrice());
-            }
-
-            
-            foreach (Vehicle v in vehicles)
-            {
-                Console.WriteLine(v.ToString());
-            }
-
-            /*
-            for (int i = 0, i < vehicles.Length; i++)
-            {
-                Vehicle v = vehicles[i];
-            }
-            */
-
             // TODAY
             // ArrayLists - Generics
             // Exceptions
@@ -85,7 +19,40 @@ namespace ConsoleApp3
             // find vehicle by id
             // EXT
             // use as and is to loop through the vehicles stored and treat each one differently
-      
+
+            Garage garage = new(3);
+
+            garage.Store(0, new Boat());
+            new Boat(); // This uses an id, then forgetten
+            garage.Store(1, new Car("REG"));
+
+            Console.WriteLine("PRINT ALL");
+            garage.PrintAll();
+
+            Console.WriteLine($"Get 1: {garage.Get(1)}");
+            Console.WriteLine($"Get 2: {garage.Get(2)}");
+            Console.WriteLine($"Get 3: {garage.Get(3)}");
+
+            garage.UseAll();
+
+            Console.WriteLine("PRINT ALL");
+            garage.PrintAll();
+
+            garage.AddSlot();
+            garage.AddSlot();
+            garage.AddSlot();
+            garage.AddSlot();
+
+            Console.WriteLine("PRINT ALL");
+            garage.PrintAll();
+
+            garage.StoreVehicle(new SunRoofCar("123"));
+            garage.StoreVehicle(new SunRoofCar("123"));
+            garage.StoreVehicle(new SunRoofCar("123"));
+
+            Console.WriteLine("PRINT ALL");
+            garage.PrintAll();
+
         }
 
 
